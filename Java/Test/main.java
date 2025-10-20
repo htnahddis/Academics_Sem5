0c0afdb130a7eb7823edc53a2a8e6f09dc82238a
Siddhanth <student.siddhanthchapade@gmail.com>
2025-10-20 22:49:19 +0530

    Code to count the mean and median of studnets in a class in java

--- Java/Test/main.java
+++ Java/Test/main.java
@@ -1,21 +1,49 @@
 import java.util.*;
+import java.util.Arrays;
+
 
 class main {
 
-    public static void main ( String[] args){
+    public static int[] School( int[] classOfStudents){
+
+        //we need to find the mode, median and mean score of the class
+        int[] stats = new int[2];
+      
+
+        //for now i will stor the mode, median and mean respectively in the stats array
+        int mean = 0;
+
+        for( int i = 0 ; i < classOfStudents.length; i++){
+            mean += i;
+        }
+
+        stats[0] = mean/(classOfStudents.length - 1);
+
+        Arrays.sort(classOfStudents);
+        
+        stats[1] = classOfStudents[ (int) classOfStudents.length/2 ];
+
+        return stats;
+    }
+
+    public static void main (String[] args) {
+
+        int[] solution = new int[2];
 
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Enter the two numbers: ");
-        int num1 = in.nextInt();
-        int num2 = in.nextInt();
+        System.out.println("What is the number of students who passed the final exam? /n");
+        int totalNumberOfPassedStudents = in.nextInt();
+                int[] arr = new int[totalNumberOfPassedStudents];
 
-        int sum = summation(num1, num2);
-        System.out.println(sum);
-    }
+        for(int i = 0 ; i < totalNumberOfPassedStudents; i++){
+            System.out.println("Enter the marks for studnet " + (i+1) + " :");
+            arr[i] = in.nextInt();
+        }
 
-    public static int summation( int a, int b){
-        return (a + b);
+        solution = School(arr);
 
+        System.out.println("The mean and median score of the class is " + solution[0] + ", " + solution[1] + ". ");
+ 
     }
 }
